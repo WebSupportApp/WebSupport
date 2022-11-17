52fb280a5b7414bf6f8c1d282862ec4e749c3b34
Ramneet Singh <singhramneet2621@gmail.com>
2022-11-17 15:25:05 -0600

    WebSockets

---
+++ models.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const clientSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
+});
+
+const ticketSchema = new mongoose.Schema({
+    client: {
+        type: clientSchema,
+        required: true
+    },
+    requestSubject: {
+        type: String,
+        required: true
+    },
+    requestDetails: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: [{
+            noteContent: String,
+            time: { type: Date, default: Date.now }
+        }],
+        default: []
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    },
+    status: {
+        type: String,
+        enum: ["New", "Acknowledged", "Waiting for Client", "Resolved"],
+        default: "New"
+    }
+});
+
+const Client = mongoose.model("Client", clientSchema);
+const Ticket = mongoose.model("Ticket", ticketSchema);
+
+module.exports = { Client, Ticket };
